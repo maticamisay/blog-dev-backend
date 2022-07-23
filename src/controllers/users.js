3f43237c3099272829cdeff066143b5e61093ddd
maticamisay <maticamisay99@hotmail.com>
2022-07-23 17:44:07 -0300

    user testing

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const express = require('express')
 const usersRouter = express.Router()
 const User = require('../models/User')
@@ -5,15 +6,16 @@ const User = require('../models/User')
 usersRouter.post('/', async (req, res) => {
     const { body } = req
     const { username, name, password } = body
+    const passwordHash = await bcrypt.hash(password, 10)
     console.log(body);
     const user = new User({
         username,
         name,
-        passwordHash: password
+        passwordHash
     })
 
     const savedUser = await user.save()
-    res.json(savedUser)
+    return res.json(savedUser)
 })
 
 module.exports = usersRouter
